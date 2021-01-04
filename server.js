be95ce0e5a171a7b6fb3105f8c59c1464cd102c0
Marek Szeles <marek.szeles@gmail.com>
2021-01-04 21:03:07 +0100

    Fixed port variable, #6 should (TM) be resolved

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/unitgenerator'));
 
-var port = 3000; // can use any port
+var port = process.env.PORT || 3000; // will use $PORT if available, otherwise 3000 (arbitrary)
 
 app.listen(port);
 console.log('Server deployed and running on port: ' + port);
